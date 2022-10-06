9f6e74edaa8ada5b470bd6a952db327a837bfa7b
tommaay <tommy@tomster.dev>
2022-10-06 06:09:30 -0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Road to Web3 - NFT Gallery</title>
+        <meta name="description" content="NFT gallery to search and display NFTs" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>NFT Gallery</main>
+    </div>
+  );
+}
